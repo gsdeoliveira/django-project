5d734c63084cd0712b1d8a6d8b764b2271c4584e
gsdeoliveira <gabriielsousa2002@gmail.com>
2022-12-18 02:57:50 -0300

    apps, render and templates

---
+++ recipes/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'recipes/index.html')
+
+
+def contato(request):
+    return HttpResponse('Contato')
+
+
+def sobre(request):
+    return HttpResponse('Sobre')
