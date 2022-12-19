9eabcf389fea3121b286e8fd2f1185cd42646f26
gsdeoliveira <gabriielsousa2002@gmail.com>
2022-12-19 08:09:42 -0300

    remove contato and sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,7 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'recipes/index.html')
-
-
-def contato(request):
-    return HttpResponse('Contato')
-
-
-def sobre(request):
-    return HttpResponse('Sobre')
+    return render(request, 'recipes/home.html')
